05d0087a7562f6e163a28248d64d045e11680a2c
winthereig00 <winthereig00@gmail.com>
2025-02-09 14:29:18 +0100

    Create Workshop 2.py

---
+++ Kursusgang 2/Workshop 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 14:16:32 2025
+
+@author: lailawinthereig
+"""
+
+"""
+Workshop 2 - Exercise 2.13
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel('Xr02-13.xlsx')
+
+data.head
